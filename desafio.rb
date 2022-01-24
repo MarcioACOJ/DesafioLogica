a7a6a2502168708fbfa83afc5d302599f2aab1eb
Marcio ACOJ <marcio.alexandre.corre.oliveira@gmail.com>
2022-01-24 20:13:45 -0300

    deleted extra lines

--- desafio.rb
+++ desafio.rb
@@ -1,11 +1,11 @@
 def contar_diamantes_e_areias(string, diamantes = 0)
     puts(string)
     return "#{diamantes} diamantes" if string == ''
-    string = string.gsub('.', '') # removendo areias
-    extraido = string.slice!('<>') # remove a primeira ocorrencia de <> da string
+    string = string.gsub('.', '') 
+    extraido = string.slice!('<>') 
     if extraido.present?
       diamantes += 1
-    else # se nao removeu é pq acabaram os diamantes
+    else 
       string = ''
     end
     contar_diamantes_e_areias(string, diamantes)
